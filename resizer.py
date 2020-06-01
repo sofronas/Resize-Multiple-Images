4c8f6d3fe2120d37dd95ac14bdcf4d7e1993694d
Konstantinos <sofronas.konstantinos@gmail.com>
2020-06-01 23:25:20 +0300

    Add files via upload

--- resizer.py
+++ resizer.py
@@ -17,7 +17,19 @@ typeOfImageUser = "empty"
 
 def UserSelectTypeOfImage():
     window = Tk()
-    window.geometry('100x100')
+    window.geometry('200x75')
+    window.title("Type")
+    windowWidth = window.winfo_reqwidth()
+    windowHeight = window.winfo_reqheight()
+    # print("Width",windowWidth,"Height",windowHeight)
+    window.iconbitmap('r.ico')
+    window.resizable(0,0)
+    # Gets both half the screen width/height and window width/height
+    positionRight = int(window.winfo_screenwidth()/2 - windowWidth/2)
+    positionDown = int(window.winfo_screenheight()/2 - windowHeight/2)
+    # Positions the window in the center of the page.
+    window.geometry("+{}+{}".format(positionRight, positionDown))
+
     def close():
         # print(variable.get())
         x = variable.get()
@@ -25,54 +37,81 @@ def UserSelectTypeOfImage():
         window.destroy()
         global typeOfImageUser
         typeOfImageUser = x
+    var = StringVar()
+    label = Label( window, textvariable=var, relief=RAISED )
+
+    var.set("Select Type of Image")
+    label.pack()
 
     variable = StringVar(window)
     variable.set("png") # default value
     w = OptionMenu(window, variable, "png", "jpg")
     w.pack()
-    closebutton = Button(window, text='X', command=close)
+    closebutton = Button(window, text='Choose', command=close)
     closebutton.pack()
     window.mainloop()
 
 
 def setPhotoParametersW():
     getWH = Tk()
+    getWH.geometry('200x100')
+    getWH.title("Width")
+    windowWidth = getWH.winfo_reqwidth()
+    windowHeight = getWH.winfo_reqheight()
+    getWH.iconbitmap('r.ico')
+    getWH.resizable(0,0)
+    # Gets both half the screen width/height and window width/height
+    positionRight = int(getWH.winfo_screenwidth()/2 - windowWidth/2)
+    positionDown = int(getWH.winfo_screenheight()/2 - windowHeight/2)
+    # Positions the window in the center of the page.
+    getWH.geometry("+{}+{}".format(positionRight, positionDown))
 
-    canvas1 = Canvas(getWH, width = 400, height = 300)
+    canvas1 = Canvas(getWH, width = 150, height = 70)
     canvas1.pack()
 
     entry1 = Entry (getWH)
-    canvas1.create_window(200, 140, window=entry1)
+    canvas1.create_window(75, 25, window=entry1)
 
     def getW ():
         x1 = entry1.get()
         getWH.destroy()
-        print(x1)
+        # print(x1)
         global width
         width = int(x1)
 
     button1 = Button(text='Get the Width', command=getW)
-    canvas1.create_window(200, 180, window=button1)
+    canvas1.create_window(75, 60, window=button1)
     getWH.mainloop()
 
 def setPhotoParametersH():
     getWH = Tk()
+    getWH.geometry('200x100')
+    getWH.title("Width")
+    windowWidth = getWH.winfo_reqwidth()
+    windowHeight = getWH.winfo_reqheight()
+    getWH.iconbitmap('r.ico')
+    getWH.resizable(0,0)
+    # Gets both half the screen width/height and window width/height
+    positionRight = int(getWH.winfo_screenwidth()/2 - windowWidth/2)
+    positionDown = int(getWH.winfo_screenheight()/2 - windowHeight/2)
+    # Positions the window in the center of the page.
+    getWH.geometry("+{}+{}".format(positionRight, positionDown))
 
-    canvas1 = Canvas(getWH, width = 400, height = 300)
+    canvas1 = Canvas(getWH, width = 150, height = 70)
     canvas1.pack()
 
     entry1 = Entry (getWH)
-    canvas1.create_window(200, 140, window=entry1)
+    canvas1.create_window(75, 25, window=entry1)
 
-    def getH ():
+    def getW ():
         x1 = entry1.get()
         getWH.destroy()
-        print(x1)
-        global height
-        height = int(x1)
+        # print(x1)
+        global width
+        width = int(x1)
 
-    button1 = Button(text='Get the Height', command=getH)
-    canvas1.create_window(200, 180, window=button1)
+    button1 = Button(text='Get the Height', command=getW)
+    canvas1.create_window(75, 60, window=button1)
     getWH.mainloop()
 
 
@@ -87,7 +126,7 @@ path = folder_selected.replace("/","\\")
 typeOfImage = '.jpg' # By default is jpg
 UserSelectTypeOfImage()
 typeOfImage = typeOfImageUser
-print("User gave: {}" .format(typeOfImageUser))
+# print("User gave: {}" .format(typeOfImageUser))
 
 filesList = []
 # r=root, d=directories, f = files
@@ -97,19 +136,21 @@ for r, d, f in os.walk(path):
             filesList.append(os.path.join(r, file))
 
 if len(filesList) == 0:
-    print("No photos\n")
+    # print("No photos\n")
     exit()
 else:
-    print("Photos Included: {}" .format(len(filesList)))
+    # print("Photos Included: {}" .format(len(filesList)))
+    pass
 
 
 pathname = os.path.dirname(sys.argv[0])
 newpath = path + r'\\resized\\'
 if not os.path.exists(newpath):
     os.makedirs(newpath)
-    print("Folder Created")
+    # print("Folder Created")
 else:
-    print("Folder already exists!")
+    # print("Folder already exists!")
+    pass
 
 # print('full path =', os.path.abspath(pathname))
 
@@ -137,7 +178,7 @@ for x in filesList:
     cv2.imwrite(location,resized)
     cv2.destroyAllWindows()
     c = c + 1
-print("Process Finished")
+# print("Process Finished")
 
 
 finishBox = Tk()
